fe092d3819b6117043f117f43ce44664f089e419
pjquinonez <pedrojesus@sbcglobal.net>
2022-04-27 10:27:29 -0700

    Cleaned Main

--- main.py
+++ main.py
@@ -2,19 +2,16 @@ import GRCalculator as gr
 import sympy as sym
 from datetime import datetime
 
-t, r, theta, phi, rs = sym.symbols("t r theta phi rs")
-g2_matrix = sym.Matrix([[-(1-(rs/r)),0,0,0],[0,1/(1-(rs/r)),0,0],[0,0,r**2,0],[0,0,0,(r**2)*(sym.sin(theta)**2)]])
+t, r, theta, phi, m, G = sym.symbols("t r theta phi m G")
+g_matrix = sym.Matrix([[-(1-((2*G*m)/r)),0,0,0],[0,1/(1-((2*G*m)/r)),0,0],[0,0,r**2,0],[0,0,0,(r**2)*(sym.sin(theta)**2)]])
+g = gr.metric(g_matrix, [t, r, theta, phi])
 
-g2 = gr.metric(g2_matrix, [t, r, theta, phi])
-chris2 = gr.christoffel(g2)
-riemann2 = gr.riemann(chris2)
+chris = gr.christoffel(g)
+riemann = gr.riemann(chris)
+
 start = datetime.now()
-print(riemann2.solve(1,2,1,2))
-# for i in range(4):
-#     for ii in range(4):
-#         for iii in range(4):
-#             print(i,ii,iii,":", chris2.solve(i,ii,iii))
+print(sym.simplify(chris.solve(0,1,0)))
+print(sym.simplify(riemann.solve(1,2,1,2)))
 print(datetime.now()-start)
-
 
 
